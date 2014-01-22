37159e787959ae11f33d95c67caef42d68c32111
rhettcp <rhettcp@gmail.com>
2014-01-22 16:26:06 -0500

    Removed Comments From Message - Rhett

--- Distributed_Part1/src/Message.java
+++ Distributed_Part1/src/Message.java
@@ -3,19 +3,9 @@ public class Message {
 	private String _ReceiverIp;
 	private String _Message;
 	private String _SenderIp;
-	/*Hi....Im Alex. My mom told me today after school we would go to the fair...But when
-	 she picked me up from school she hit me with a wrench....Twice...no actually it was
-	 three times...I got hit so many times that i tend to forget...but when I got home 
-	 she sat me down and asked me "Alex, do you know why I beat you with a wrench?" and 
-	 then I said....
-	 */
-	//Rhett Test.
-	//This is a test from Paul .
-	//Construct.
+
 	public Message(){
-		//Rhett Pushed A Big'en.
-		//Clint added this
-		//Clint added this too
+
 	}
 	
 	public void setReceiverIp(String receiver){
