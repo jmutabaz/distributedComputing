079304b953bab570228ec6ad281982ba1f7496a6
rhettcp <rhettcp@gmail.com>
2014-01-22 14:34:50 -0500

    Added A Message Class - Rhett

---
+++ Distributed_Part1/src/Message.java
@@ -0,0 +1,36 @@
+
+public class Message {
+	private String _ReceiverIp;
+	private String _Message;
+	private String _SenderIp;
+	
+	//Construct.
+	public Message(){
+		
+	}
+	
+	public void setReceiverIp(String receiver){
+		_ReceiverIp = receiver;
+	}
+	
+	public void setMessage(String msg){
+		_Message = msg;
+	}
+	
+	public void setSenderIp(String sender){
+		_SenderIp = sender;
+	}
+	
+	public String getReceiverIp(){
+		return _ReceiverIp;
+	}
+	
+	public String getMessage(){
+		return _Message;
+	}
+	
+	public String getSenderIp(){
+		return _SenderIp;
+	}
+	
+}
