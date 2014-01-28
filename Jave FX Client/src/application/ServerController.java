ff578e574ee0c16f9563f9bce29f6f031357d446
John Mutabazi <jmutabaz@gmail.com>
2014-01-28 01:15:47 -0500

    John
    Created the 'serverWindow' GUI for now

--- Jave FX Client/src/application/ServerController.java
+++ Jave FX Client/src/application/ServerController.java
@@ -1,5 +1,25 @@
 package application;
 
-public class ServerController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ButtonBase;
+import javafx.scene.control.TextArea;
+
+public class ServerController implements Initializable, ControlledScreen {
+	
+	ScreensController myController;
+	
+	@FXML	ButtonBase startStopServer;
+	@FXML	TextArea messageArea;
+	
+	@FXML
+	public void startOrStopServer(ActionEvent event){
+		
+		messageArea.setText("Awesome ninja");
+	}
+	
 }
