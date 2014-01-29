2f4ff7b69e805f1ae13047f2ac6863fccb09999f
Clint Walker <walker.clint@icloud.com>
2014-01-29 18:13:06 -0500

    Clint
    gui updates

--- Jave FX Client/src/application/ClientController.java
+++ Jave FX Client/src/application/ClientController.java
@@ -3,62 +3,60 @@ package application;
 import java.io.File;
 import java.io.IOException;
 import java.net.SocketException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import Model.SocketClient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
-public class ClientController {
+public class ClientController implements Initializable, ControlledScreen {
+	ScreensController myController;
+	
+	@FXML	TextArea	messageWindowArea;
+	@FXML	TextField	serverRouterIPAddressField;
+	@FXML	TextField	serverIPAddressField;	
+	@FXML	TextField	portNumberField;
+	@FXML	Button		exitButton;
+	@FXML	Button		startClientButton;
+	@FXML	Button		loadFilebButton;
+	@FXML	Button		sendMessageButton;
+	
+	
+	
+	private String serverRouterIPAddressString;
+	private String serverIPAddressString;
+	private String clientIPAddressString;
+	private int portNumber;
+	
+	private SocketClient		cl								= new SocketClient();
+	private String				fileNameString					= null;
+	private boolean setup = false,
+					fileLoaded;
 
-	/*@FXML
-	public void connect(ActionEvent event){
-		if (setup){
-			//get ip address, port number and send to client class to establish connection
-			serverRouterIPAddressString = serverRouterIPAddressField.getText();
-			serverIPAddressString = serverIPAddressField.getText();
-			clientIPAddressString = clientIPAddressField.getText();
-			portNumber =  Integer.parseInt(portsNumberField.getText());
-			
-			if (server){
-				// send ip addresses and port number to server class
-				try {
-					ser.RunTranslationServer(serverRouterIPAddressString, portNumber, clientIPAddressString);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					System.out.print("failed to start server (catch statement in start menu controller connect method");
-				}
-			
-			} else {
-				// send ip addresses and port number to client class
-				if (loadFile(event)){;
-					try {
-						cl.RunClient(serverRouterIPAddressString, portNumber, serverIPAddressString, fileNameString);
-					} catch (SocketException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						System.out.print("\nFailed to start client (catch statement in start menu controller connect method)");
-					}
-				}
-				
-			}
-		// if connection established then switch to Chat Window
-		}
+	@Override
+	public void setScreenParent(ScreensController screenPage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	@FXML
-	public boolean loadFile(ActionEvent event){
-		fileChooser = new FileChooser();
-		File file = fileChooser.showOpenDialog(Main.PRIMARYSTAGE_STAGE);
-		if (file != null) {
-            //openFile(file);
-        	fileNameString = file.getAbsolutePath();
-        	System.out.println("filename = " + fileNameString);
-        	if (fileNameString != null){
-        		return true;
-        	}
-        }
-		return false;
-	}*/
+	public void exitToStart(ActionEvent event){
+		Main.PRIMARYSTAGE_STAGE.setHeight(300);
+		Main.PRIMARYSTAGE_STAGE.setWidth(600);
+		myController.setScreen(Main.START_MENU);
+	}
+
 	
 }
