b4c592065d23be120d8fa9660721dcbcae9cddae
rhettcp <rhettcp@gmail.com>
2014-04-10 21:06:43 -0400

    Revert "Updated Sending"
    This reverts commit 8497dcccd6d4dc7638ef2a816a9fe4c64a27ae03.

--- Proj 2 Team/Proj_2_Distributed_Computing/application/ClientScreenController.java
+++ Proj 2 Team/Proj_2_Distributed_Computing/application/ClientScreenController.java
@@ -64,8 +64,8 @@ public class ClientScreenController implements Initializable, ControlledScreen {
 										fileNameString					= "",
 										messageToSendString				= "";
 	private 		FileChooser			fileChooser 					= new FileChooser();
-	private			SocketClient		clientConn						= null;
-	private			SocketClient		serverConn						= null;
+	private			SocketClient		clientConn						;
+	private			SocketClient		serverConn						;
 
 	private			ArrayList<String> 	list;
 
@@ -142,18 +142,14 @@ public class ClientScreenController implements Initializable, ControlledScreen {
 		if (messgaeToSendArea.getText() != ""){
 			msg.setData(messgaeToSendArea.getText()); // the string to send
 			msg.setType(true); // true if string false if file
-			clientConn = new SocketClient(Main.IPADDRESSSTRING, serverRouterIPAddressField.getText(), nameOfRecievingClientField.getText(), 2, msg);
-			clientConn.start();
+			//clientConn.RunClient(msg.getDestination(), 5555, msg);
 		// if sending a message
 		//do
 		} else {
 			if (fileNameString != ""){
 			msg.readFileIntoData(fileNameString); // file to send
 			msg.setType(false);
-			//BANANA - Get File Name
-			msg.setFileName("NewFile.jpg");
-			clientConn = new SocketClient(Main.IPADDRESSSTRING, serverRouterIPAddressField.getText(), nameOfRecievingClientField.getText(), 2, msg);
-			clientConn.start();
+			//clientConn.RunClient(msg.getDestination(), 5555, msg);
 			}
 		}
 		//
